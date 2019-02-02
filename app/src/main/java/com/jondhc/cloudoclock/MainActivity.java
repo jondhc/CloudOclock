075758524429354c641664c8e0612d10be4df562
J. Daniel Herrejón C <jonathandaniel.heca@gmail.com>
2019-02-01 22:49:23 -0600

    Getting local time.

--- app/src/main/java/com/jondhc/cloudoclock/MainActivity.java
+++ app/src/main/java/com/jondhc/cloudoclock/MainActivity.java
@@ -2,6 +2,10 @@ package com.jondhc.cloudoclock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        setLocalTime();
+    } //end onCreate
+
+    public void setLocalTime(){
+        TextView localTime = findViewById(R.id.localTime);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        String formattedTime = timeFormat.format(c.getTime());
+        localTime.setText(formattedTime);
+    } //end setLocalTime
+
+
+
+
+} //end MainActivity
